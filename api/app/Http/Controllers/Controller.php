8467b51146118c62d7c0535540798e5c484708f7
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-04-02 21:03:15 +0300

    commit for api response method

--- api/app/Http/Controllers/Controller.php
+++ api/app/Http/Controllers/Controller.php
@@ -1,8 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
 
 abstract class Controller
 {
     //
+
+    public function respond ($data) : JsonResponse
+    {
+        return response()->json($data);
+    }
 }
