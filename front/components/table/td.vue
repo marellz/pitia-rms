8c5336225fdf7566d26cb8c025b8e818f0081fac
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-04-04 13:43:08 +0300

    table component

---
+++ front/components/table/td.vue
@@ -0,0 +1,13 @@
+<template>
+  <td
+    class="border-b first:border-l last:border-r align-middle border-gray-200 px-3 py-4 text-gray-700"
+    @click="() => $emit('click')"
+  >
+    <span>
+      <slot />
+    </span>
+  </td>
+</template>
+<script setup>
+defineEmits(["click"]);
+</script>
---
+++ front/components/table/tr.vue
@@ -0,0 +1,13 @@
+<template>
+  <tr :class="{ 'hover:bg-blue-100': hover }">
+    <slot />
+  </tr>
+</template>
+<script setup>
+defineProps({
+  hover: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
