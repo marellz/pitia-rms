80c611be5be25a2ece17b6792713da93bd2139e9
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-04-04 23:17:05 +0300

    update alert component

--- front/components/custom/alert.vue
+++ front/components/custom/alert.vue
@@ -9,41 +9,32 @@
   </div>
 </template>
 <script lang="ts" setup>
-enum VariantsEnum {
-  SUCCESS = "success", // green
-  INFO = "info", // blue
-  WARNING = "warning", // amber
-  DANGER = "danger", // red
-}
-
-let variantArr = ["success"];
-
 interface VariantColorTypes {
   bg: string;
-  title?: string;
+  title: string;
 }
 
 interface VariantTypes {
-  [VariantsEnum.SUCCESS]: VariantColorTypes;
-  [VariantsEnum.INFO]: VariantColorTypes;
-  [VariantsEnum.WARNING]: VariantColorTypes;
-  [VariantsEnum.DANGER]: VariantColorTypes;
+  success: VariantColorTypes
+  info : VariantColorTypes
+  warning : VariantColorTypes
+  danger : VariantColorTypes
 }
 
 const variants: VariantTypes = {
-  [VariantsEnum.SUCCESS]: { bg: "bg-green-200", title: "text-green-600" },
-  [VariantsEnum.INFO]: { bg: "bg-blue-200", title: "text-blue-600" },
-  [VariantsEnum.WARNING]: { bg: "bg-amber-200", title: "text-amber-600" },
-  [VariantsEnum.DANGER]: { bg: "bg-red-200", title: "text-red-500" },
+  success: { bg: "bg-green-200", title: "text-green-600" },
+  info: { bg: "bg-blue-200", title: "text-blue-600" },
+  warning: { bg: "bg-amber-200", title: "text-amber-600" },
+  danger: { bg: "bg-red-200", title: "text-red-500" },
 };
 
 const props = withDefaults(
   defineProps<{
     title?: string;
-    variant: string;
+    variant: "info" | "warning" | "danger" | "success";
   }>(),
   {
-    variant: "warning",
+    variant: "info",
   }
 );
 
