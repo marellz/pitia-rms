2184865d8c1e507efbad46e85651accababaa95c
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-04-11 12:14:46 +0300

    update order layput

--- front/layouts/order.vue
+++ front/layouts/order.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="h-[100vh] flex flex-col">
     <header class="py-5 border-b">
-      <layout-container class="flex items-center">
-        <div class="flex-auto">
-            <nuxt-link to="/" class="font-bold text-2xl"> PitiaRMS </nuxt-link>
-        </div>
-        <nuxt-link to="/" class="font-medium bg-gray-100 rounded py-2 px-5 text-blue">
+      <layout-container class="flex items-center justify-center relative">
+         <nuxt-link to="/" class="font-medium bg-gray-100 rounded py-2 px-5 text-blue inline-flex items-center space-x-2 text-lg absolute left-5">
+          <ArrowLeftIcon class="h-6"/>
+          <span class="hidden lg:block">
             Dashboard
+          </span>
         </nuxt-link>
+        <nuxt-link to="/" class="font-bold text-2xl mx-auto"> PitiaRMS </nuxt-link>
       </layout-container>
     </header>
     <main class="py-10 flex-auto flex flex-col">
@@ -15,4 +16,6 @@
     </main>
   </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ArrowLeftIcon } from '@heroicons/vue/24/outline';
+</script>
