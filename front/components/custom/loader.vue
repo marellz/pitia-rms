aee1844517eaaa74471ddc767c5a51661399a676
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-04-22 14:40:18 +0300

    add loader

---
+++ front/components/custom/loader.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="py-5 my-5 text-center">
+    <div class="loader inline-flex"></div>
+</div>
+</template>
+<script lang="ts" setup>
+</script>
+<style lang="scss">
+.loader {
+  width: 65px;
+  aspect-ratio: 1;
+  position: relative;
+}
+.loader:before,
+.loader:after {
+  content: "";
+  position: absolute;
+  border-radius: 50px;
+  box-shadow: 0 0 0 3px inset rgba(0,0,0, .1);
+  animation: l4 2.5s infinite;
+}
+.loader:after {
+  animation-delay: -1.25s;
+}
+@keyframes l4 {
+  0% {
+    inset: 0 35px 35px 0;
+  }
+  12.5% {
+    inset: 0 35px 0 0;
+  }
+  25% {
+    inset: 35px 35px 0 0;
+  }
+  37.5% {
+    inset: 35px 0 0 0;
+  }
+  50% {
+    inset: 35px 0 0 35px;
+  }
+  62.5% {
+    inset: 0 0 0 35px;
+  }
+  75% {
+    inset: 0 0 35px 35px;
+  }
+  87.5% {
+    inset: 0 0 35px 0;
+  }
+  100% {
+    inset: 0 35px 35px 0;
+  }
+}
+</style>
+
